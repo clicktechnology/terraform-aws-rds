8dc3293b85927f83e833ca73b42183c8e46710d5
Morgan Conlon <morgan.conlon@click-technology.com>
2024-04-18 21:27:45 +0100

    Update default values for db_username and db_password in variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,16 @@ variable "db_username" {
   description = "RDS root username"
 }
 
+# value is set as an env variable in Terraform cloud
+# TF_VAR_db_username=xxxxxxxxxxxx
+
 variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+# value is set as an env variable in Terraform cloud
+# TF_VAR_db_password=xxxxxxxxxxxx
 
 variable "AWS_SECRET_ACCESS_KEY" {
   default = ""
